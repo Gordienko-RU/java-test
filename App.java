2e013ff3c0fd02f1b3a50a4fdefee1ef52039316
Gordienko-RU <r.u.gordienko@gmail.com>
2022-10-20 00:28:31 +0300

    up

--- App.java
+++ App.java
@@ -1,70 +1,60 @@
-import java.util.Collection;
+import java.util.ArrayDeque;
 import java.util.Date;
-import java.util.TreeMap;
+import java.util.Iterator;
 
 public class App {
   public static void main(String[] args) {
-    /**
-     * why did we have '?' for mean method?\ - have to pick correct type(some
-     * dynamic type, or sort of probably)
-     * what about accessing from several threads
-     * optimize slicing buy provide comparator
-     * test
-     */
     class Consumer {
-      private TreeMap<Long, Integer> data = new TreeMap<Long, Integer>();
+      static final class DataPair {
+        public int number;
+        public long timestamp;
+
+        public DataPair(int number, long timestamp) {
+          this.number = number;
+          this.timestamp = timestamp;
+        }
+      }
+
+      private ArrayDeque<DataPair> data = new ArrayDeque<DataPair>();
       private static final int EXPIRATION_PERIOD_MS_OFFSET = 300000;
 
-      private Long getOldestNotExpiredDataKey(long ts) {
-        long oldestValidTSValue = ts - Consumer.EXPIRATION_PERIOD_MS_OFFSET;
-
-        return this.data.ceilingKey(oldestValidTSValue);
-      }
-
-      private boolean isThereDataToAggregate(long ts) {
-        Long oldestNotExpiredDataKey = this.getOldestNotExpiredDataKey(ts);
-
-        return oldestNotExpiredDataKey != null ? true : false;
-      }
-
-      private Collection<Integer> getAggregableValues(long ts) {
-        long oldestNotExpiredDataKey = this.getOldestNotExpiredDataKey(ts);
-        long lastKey = this.data.lastKey();
-
-        return this.data
-            .subMap(oldestNotExpiredDataKey, true, lastKey, true)
-            .values();
+      private long buildOldestValidTimestamp() {
+        return new Date().getTime() - Consumer.EXPIRATION_PERIOD_MS_OFFSET;
       }
 
       public void accept(int number) {
-        Long key = new Date().getTime();
-        System.out.println(key);
-        this.data.put(key, number);
+        Long timestamp = new Date().getTime();
+
+        Consumer.DataPair pair = new Consumer.DataPair(number, timestamp);
+        this.data.addFirst(pair);
       }
 
       public long mean() {
-        long ts = new Date().getTime();
-        boolean thereIsDataToAggregate = this.isThereDataToAggregate(ts);
-
-        if (!thereIsDataToAggregate) {
-          return 0;
-        }
-
-        Collection<Integer> aggregableValues = this.getAggregableValues(ts);
+        long oldestValidTimestamp = this.buildOldestValidTimestamp();
+        long amount = 0;
         long sum = 0;
+        Iterator<Consumer.DataPair> iterator = this.data.iterator();
 
-        for (Integer number : aggregableValues) {
-          sum += number;
+        while (iterator.hasNext()) {
+          Consumer.DataPair pair = iterator.next();
+
+          if (pair.timestamp >= oldestValidTimestamp) {
+            sum += pair.number;
+            ++amount;
+          } else {
+            break;
+          }
         }
 
-        return sum / aggregableValues.size();
+        return amount == 0 ? 0 : sum / amount;
       }
     }
 
     Consumer consumer = new Consumer();
 
-    consumer.accept(3);
-    consumer.accept(2);
+    consumer.accept(11);
+    consumer.accept(7);
+    consumer.accept(7);
     consumer.accept(7);
     System.out.println(consumer.mean());
   }
